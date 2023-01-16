6b076e2d5e59dc9e04242e91319a92470b02a0f5
JuanFlorez1326 <juanks339@gmail.com>
2023-01-16 13:40:00 -0500

    Country view screen ended

--- src/app/countries/pages/see-country/see-country.component.ts
+++ src/app/countries/pages/see-country/see-country.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { CountryService } from '../../services/country.service';
+import { Country, Name } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-see-country',
   templateUrl: './see-country.component.html'
 })
-export class SeeCountryComponent {
+export class SeeCountryComponent implements OnInit {
+  
+  constructor(
+    private readonly countryService: CountryService,
+    private readonly activateRoute: ActivatedRoute
+  ) {}
 
+  country!: Country;
+  
+  ngOnInit(): void {
+    this.activateRoute.params
+    .pipe(
+      switchMap( ({ id }) =>
+        this.countryService.getContryByCode( id )
+      ), tap( console.log )
+    )
+    .subscribe( res => {
+      this.country = res[0];
+    })
+  }
 }
